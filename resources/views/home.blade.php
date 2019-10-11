33b0571f4b345af0c786103d26224089be957ddf
Menawer <careless.kw@gmail.com>
2019-10-11 17:05:35 +0300

    C5 L5 V5 R1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @for ($i = 0; $i < count($books); $i++)
-        {{$books[$i]->title }} هذا عنوان الكتاب<br>
-    @endfor
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title','صفحه الكتب')
+
+@section('content')
+
+@forelse ($books as $book)
+    {{ $loop->index }} رقم الدوره<br>
+    {{$book->title}} عنوان الكتاب هو<br>
+    {{$book->pages}} عدد صفحاته هو<br>
+    @empty
+        لا توجد كتب
+@endforelse
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>App name - @yield('title')</title>
+</head>
+<body>
+    <header>Header يكون هنا</header>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <footer>Footer يكون هنا</footer>
+</body>
+</html>
