c4c48eddd6788dda815e0467b8a718daa0da8d83
Menawer <careless.kw@gmail.com>
2019-10-13 20:55:36 +0300

    C6 L4 V2 R1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>App name - @yield('title')</title>
 </head>
 <body>
+    <!-- As a link -->
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
     <header>
         <a href="/toArabic">العربية</a>
         <a href="/toEnglish">English</a>
@@ -27,5 +32,6 @@
     </div>
 
     <footer>Footer يكون هنا</footer>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
